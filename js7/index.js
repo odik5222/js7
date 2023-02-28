8e8b69566c5f6ce395757723938bc1a76fd2fb15
Odik-web <116338011+odik5222@users.noreply.github.com>
2023-02-28 11:43:24 +0500

    Add files via upload

---
+++ js7/index.js
@@ -0,0 +1,17 @@
+let num = 5
+let str ='hello'
+let nan = num/str
+console.log(num);
+let hello;
+console.log(hello);
+let n1 = null
+console.log(typeof(hello));
+console.log(typeof(n1));
+let inf = BigInt(5457094989849804 * 4324323462347)
+
+console.log(inf);
+console.log(number.isInteger(inf));
+
+console.log(typeof(inf));
+const theBiggest = 90943843437349887n
+console.log(theBiggest);
